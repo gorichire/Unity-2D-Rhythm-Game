4fd85a9a28bb51c1372a07cfa126e271bbc7c22e
gorichire <168408037+gorichire@users.noreply.github.com>
2025-01-11 07:16:30 +0900

    판정 가이드 및 판정 선 생성

---
+++ Unity 2D Rhythm Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    // ΩÃ±€≈Ê
+    public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+    }
+
+    // ≥Î∆Æ º”µµ
+    public float notespeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
