cb18280672ad00e9d599393ea23000ff63c28a6a
gorichire <168408037+gorichire@users.noreply.github.com>
2025-03-10 05:57:13 +0900

    결제 데이터 관리

--- Unity 2D Rhythm Game/Assets/Scripts/NoteController.cs
+++ Unity 2D Rhythm Game/Assets/Scripts/NoteController.cs
@@ -127,8 +127,7 @@ public class NoteController : MonoBehaviour
     void AddRank()
     {
         // 데이터베이스 접속 설정하기
-        DatabaseReference reference;
-        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        DatabaseReference reference = PlayerInformation.GetDatabaseReference();
         // 삽입할 데이터 준비하기
         DateTime now = DateTime.Now.ToLocalTime();
         TimeSpan span = (now - new DateTime(1970, 1, 1, 0, 0, 0).ToLocalTime());
--- Unity 2D Rhythm Game/Assets/Scripts/PlayerInformation.cs
+++ Unity 2D Rhythm Game/Assets/Scripts/PlayerInformation.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Database;
 
 public static class PlayerInformation
 {
@@ -11,5 +13,12 @@ public static class PlayerInformation
     public static string musicTitle { get; set; }
     public static string musicArtist { get; set; }
     public static Firebase.Auth.FirebaseAuth auth;
+
+    public static DatabaseReference GetDatabaseReference()
+    {
+        DatabaseReference reference;
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        return reference;
+    }
 
 }
